b6d0c3812214ab6db56a1f44e9bdda62bf08cdb3
dev <dev@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2012-03-29 03:25:41 +0000

    1st commit

---
+++ public/js/parsexml.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	
+	Progress = $('#progress');
+	Errors   = $('#parseerrors');
+	
+	$('#parsechannels').click(function(e){
+		e.stopPropagation();
+		$.ajax({
+			url: '/admin/import/xmlparsechannels/ajax',
+			data: $('form#parse_channels').serialize(),
+			dataType: 'html',
+			type: 'post',
+			beforeSend:function(jqXHR, settings){
+				Progress.hide().html('<p>Начинаю парсинг каналов из XML</p>').fadeIn();
+			},
+			success: function(response, textStatus, jqXHR){
+				Progress.fadeOut().html(Progress.html()+'<p>'+response+'</p>').fadeIn();
+				return true;
+				//$(this).abort();
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				Progress.hide().html(Progress.html()+'<p>'+errorThrown+'</p>').fadeIn();
+				return false;
+				//$(this).abort();
+			}
+		});
+		$('#submitbutton').fadeOut();
+		return false;
+	});
+	
+	$('#parseprograms').click(function(e){
+		e.stopPropagation();
+		$.ajax({
+			url: '/admin/import/xmlparseprograms/ajax',
+			data: $('form#parse_programs').serialize(),
+			dataType: 'html',
+			type: 'post',
+			beforeSend:function(jqXHR, settings){
+				Progress.hide().html('<p>Начинаю парсинг программ из XML</p>').fadeIn();
+			},
+			success: function(response, textStatus, jqXHR){
+				Progress.fadeOut().html(Progress.html()+'<p>'+response+'</p>').fadeIn();
+				return true;
+				//$(this).abort();
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				Progress.hide().html(Progress.html()+'<p>'+errorThrown+'</p>').fadeIn();
+				return false;
+				//$(this).abort();
+			}
+		});
+		$('#submitbutton').fadeOut();
+		return false;
+	});
+});
