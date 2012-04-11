d764fad4ef36848d9d23acc63bef11ba296798de
dev <dev@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2012-04-11 08:27:21 +0000

    *** empty log message ***

--- public/js/schedule-processing.js
+++ public/js/schedule-processing.js
@@ -1,11 +1,58 @@
 $(document).ready(function(){
-	$('#submitbutton').click(function(){
+	
+	$('#start-search').click(function(){
 		$.ajax({
 			url: '/admin/programs/premieres-search',
 			data: $('#premiere_search').serialize()+'&format=html',
 			dataType: 'html',
+			beforeSend: function(){
+				$('#result').html("<p>Происходит удаление программ</p>");
+			},
 			success: function(response){
-				$('#result').html(response);
+				$('#result').css({ "background-color":"lightGreen" }).html(response);
+			},
+			error: function(XHR, textStatus, errorThrown){
+				$('#result').css({ "background-color":"lightRed" }).html(errorThrown);
+			}
+		});
+	});
+	
+	$('#delete-programs').change(function(){ 
+		if ($(this).prop('checked')==true && $('input#delete-info').prop('checked')==false) {
+			$('input#delete-info').prop('checked', true);
+		}
+	});
+
+	$('#titles_parsing').click(function(){ 
+		$.ajax({
+			url: '/admin/import/parse-saved-programs',
+			data: $('#parse_titles').serialize()+'&format=html',
+			dataType: 'html',
+			beforeSend: function(){
+				$('#result').css({ 'background':'none' }).html("<p>Происходит обработка программ</p>");
+			},
+			success: function(response){
+				$('#result').css({ "background-color":"lightGreen" }).html(response);
+			},
+			error: function(XHR, textStatus, errorThrown){
+				$('#result').css({ "background-color":"lightRed" }).html(errorThrown);
+			}
+		});
+	});
+	
+	$('#delete_start_btn').click(function(){
+		$.ajax({
+			url: '/admin/programs/delete-programs',
+			data: $('#delete_programs').serialize()+'&format=html',
+			dataType: 'html',
+			beforeSend: function(){
+				$('#result').html("<p>Происходит удаление программ</p>");
+			},
+			success: function(response){
+				$('#result').css({ "background-color":"lightGreen" }).html(response);
+			},
+			error: function(XHR, textStatus, errorThrown){
+				$('#result').css({ "background-color":"lightRed" }).html(errorThrown);
 			}
 		});
 	});
