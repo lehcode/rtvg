53e57ff3fc57a12e25c5819096291e01aee57064
developer <developer@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2013-01-04 01:08:05 +0000

    *** empty log message ***

---
+++ application/forms/TypeaheadForm.php
@@ -0,0 +1,74 @@
+<?php
+class Xmltv_Form_TypeaheadForm extends Zend_Form
+{
+	
+	private $_formClass='typeahead';
+	private $_formCss='';
+	private $_actionUrl='';
+	private $_labelClass='mediumheading';
+	
+    public function __construct($options = null) {
+    	
+        parent::__construct($options);
+		
+		if (isset($options['form_class']) && !empty($options['form_class'])) {
+			$this->_formClass = $options['form_class'];
+		}
+		if (isset($options['form_css']) && !empty($options['form_css'])) {
+			$this->_formCss = $options['form_css'];
+		}
+		if (isset($options['action_url']) && !empty($options['action_url'])) {
+			$this->_actionUrl = $options['action_url'];
+		}
+		if (isset($options['label_class']) && !empty($options['label_class'])) {
+			$this->_labelClass = $options['label_class'];
+		}
+		
+		$search = new Zend_Form_Element_Text('channel');
+		$decorators = array(
+            array('ViewHelper'),
+            array('Errors'),
+            array('Label', array('tag' => 'div', 'class' => $this->_labelClass )),
+        );
+		
+		$search->setLabel("Быстрый поиск по телеканалам")
+			->setAttrib('data-provide', 'typeahead')
+			->addFilter('StripTags')
+			->addValidator('NotEmpty')
+    		->addFilter('StringTrim')
+    		->setDecorators($decorators)
+    		->removeDecorator('HtmlTag');
+		
+    	$submit = new Zend_Form_Element_Submit('submit');
+		$decorators = array(
+            array('ViewHelper'),
+            array('HtmlTag', array('tag' => 'span')),
+        );
+    	$submit->setLabel('Перейти')
+			->setAttrib('class', 'btn btn-mini btn-primary')
+			->setDecorators($decorators)
+			->removeDecorator('HtmlTag');
+			
+		$view = new Zend_View();
+		$this->addElements(array($search, $submit));
+		$formAttribs = array(
+			'action'=>$view->url(array(), 'default_search_search'),
+			'method'=>'post',
+			'enctype'=>'application/x-www-form-urlencoded',
+			'name'=>'typeahead',
+			//'class'=>$this->_formClass,
+		);
+		if (!empty($this->actionUrl)) {
+			$formAttribs['action'] = $this->actionUrl;
+		}
+		$this->setAttribs($formAttribs);
+		$this->setDecorators(array(
+				'FormElements',
+				'Form', array(
+					array('data'=>'HtmlTag'),
+					array('tag'=>'div', 'class'=>$this->_formClass)
+				),
+			));
+		
+    }
+}
