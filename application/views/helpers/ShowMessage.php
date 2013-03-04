89267fd573619f0c93e8158fb7bec3a65183cb9f
developer <developer@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2013-03-04 20:35:08 +0000

    since beta3

---
+++ application/views/helpers/ShowMessage.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 
+ * Helper to display notice/alert/error message
+ * 
+ * @author  Antony Repin <egeshisolutions@gmail.com>
+ * @package sosedionline
+ * @filesource $Source: /home/developer/cvs/rutvgid.ru/application/views/helpers/ShowMessage.php,v $
+ * @version $Id: ShowMessage.php,v 1.1 2013-03-04 20:35:08 developer Exp $
+ */
+class Zend_View_Helper_ShowMessage extends Zend_View_Helper_Abstract
+{
+    /**
+     * Show message on page
+     * 
+     * @param  string $msg
+     * @param  string $type // 'message'|'warning'|'error'
+     * @return string
+     */
+	public function showMessage($msg=null, $type='message'){
+		
+		switch ($type) {
+			default:
+			case 'message':
+				$heading = 'Сообщение';
+				break;
+			case 'warning':
+				$heading = 'Предупреждение';
+				break;
+			case 'error':
+				$heading = 'Ошибка';
+				break;
+		}
+		ob_start();
+		?>
+		<div class="alert alert-block">
+			<a class="close" data-dismiss="alert" href="#">×</a>
+			<h4 class="alert-heading"><?php echo $heading ?></h4>
+			<?php echo $msg; ?>
+		</div>
+		<?php 
+		return ob_get_clean();
+		
+	}
+}
