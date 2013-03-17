1eb3225a73c4abf00b673bda8820064827cc5381
developer <developer@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2013-03-17 17:14:27 +0000

    since beta5

---
+++ application/modules/admin/views/helpers/TruncateString.php
@@ -0,0 +1,29 @@
+<?php
+
+class Zend_View_Helper_TruncateString extends Zend_View_Helper_Abstract
+{
+
+	public function truncateString ($string, $length=10, $mode='letters', $end='…') {
+				
+		switch ($mode) {
+			case 'words':
+				$parts = explode(' ', $string);
+				$c = count($parts)-1;
+				if (count($parts)>$length) {
+					do {
+						unset($parts[$c]);
+						$c--;
+					} while (count($parts)>$length);
+					return rtrim( implode(' ', $parts), '.–;:?,!').$end;
+				} else {
+					return $string;
+				}
+				break;
+			default:
+				if (Xmltv_String::strlen($string)<=$length)
+				return $string;
+				else
+				return Xmltv_String::substr($string, 0, $length-1).'…';
+		}
+	}
+}
