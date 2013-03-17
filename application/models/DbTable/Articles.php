9028281ced137be4a017b860dcb4fe4e87c71c1f
developer <developer@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2013-03-17 06:43:33 +0000

    since beta5

---
+++ application/models/DbTable/Articles.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Frontend articles model
+ * 
+ * @author     Antony Repin <egeshisolutions@gmail.com>
+ * @subpackage frontend
+ * @version    $Id: Articles.php,v 1.1 2013-03-17 06:43:33 developer Exp $
+ *
+ */
+class Xmltv_Model_DbTable_Articles extends Xmltv_Model_Abstract
+{
+	
+	public function init()
+	{
+		parent::init();
+	}
+	
+}
