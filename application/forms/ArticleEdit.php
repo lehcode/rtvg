e93ba8b30a15f221d8f2f95b516e08dc500493c5
developer <developer@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2013-03-17 21:33:50 +0000

    since beta5

---
+++ application/forms/ArticleEdit.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Редактирование статьи
+ * 
+ * @author     Antony Repin
+ * @subpackage backend
+ * @version    $Id: ArticleEdit.php,v 1.1 2013-03-17 21:33:50 developer Exp $
+ *
+ */
+class Xmltv_Form_ArticleEditForm extends Zend_Form
+{
+	
+}
