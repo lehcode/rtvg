a71e9b63e700642b4dd383c3a2a20a3993c65ca8
developer <developer@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2013-03-18 18:21:32 +0000

    since beta5

---
+++ application/forms/EditArticle.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Редактирование статьи
+ * 
+ * @author     Antony Repin
+ * @subpackage backend
+ * @version    $Id: EditArticle.php,v 1.1 2013-03-18 18:21:32 developer Exp $
+ *
+ */
+class Xmltv_Form_EditArticle extends Zend_Form
+{
+	
+}
