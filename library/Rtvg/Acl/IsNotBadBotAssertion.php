0beae3e012e284f315a591abc614feb826949fcc
developer <developer@023b655a-0172-11e3-96d2-2d5d0183a5b2>
2013-03-21 20:42:38 +0000

    since beta5

---
+++ library/Rtvg/Acl/IsNotBadBotAssertion.php
@@ -0,0 +1,76 @@
+<?php
+class Rtvg_Acl_IsNotBadBotAssertion implements Zend_Acl_Assert_Interface
+{
+    
+    private $_userAgent;
+    
+    /**
+     * (non-PHPdoc)
+     * @see Zend_Acl_Assert_Interface::assert()
+     */
+    public function assert( Zend_Acl $acl,
+    		Zend_Acl_Role_Interface $role = null,
+    		Zend_Acl_Resource_Interface $resource = null,
+    		$privilege = null )
+    {
+        
+        //die(__FILE__.': '.__LINE__);
+        
+        $this->_userAgent = Zend_Controller_Front::getInstance()
+        	->getParam('bootstrap')
+        	->getResource('useragent');
+        $userAgent = $this->_userAgent->getUserAgent();
+        $badAgents = array(
+        	'Windows NT 6.1',
+        	'Mozilla/4.0 (compatible; MSIE 4.01; Windows NT)',
+        	'Mozilla/5.0 (compatible; SearchBot)',
+        	'Mozilla/4.61 [en] (X11; U; ) - BrowseX (2.0.0 Windows)',
+        );
+        foreach ($badAgents as $bad){
+        	if ($userAgent==$bad) {
+        	    //die(__FILE__.': '.__LINE__);
+        	    $this->_exception();
+        	}
+        }
+        
+        $badAgents = array(
+        	'libwww-perl',
+        	'MJ12bot',
+        	'Nutch',
+        	'cr4nk',
+        	'pycurl',
+        );
+        foreach ($badAgents as $string){
+        	if (stristr($userAgent, $string)) {
+        		$this->_exception();
+        	}
+        }
+        
+        //var_dump($userAgent);
+        //die(__FILE__.': '.__LINE__);
+        
+        // Check by browser type
+        $browserType = $this->_userAgent->getBrowserType();
+        
+        $allowedTypes = array( 'desktop', 'mobile', 'validator', 'feed', null, '' );
+        if (!in_array($browserType, $allowedTypes) ){
+            $this->_exception();
+        }
+        
+        $badIps = array(
+        	'46.164.176.66',
+        	'38.99.82.243',
+        );
+        if (in_array($_SERVER['HTTP_HOST'], $badIps)) {
+        	$this->_exception();
+        }
+        
+        return true;
+        
+    }
+    
+    private function _exception(){
+        throw new Zend_Exception( Rtvg_Message::ERR_NO_AUTH, 401 );
+    }
+    
+}
