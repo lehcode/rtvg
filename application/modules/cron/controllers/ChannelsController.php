15a24cb76284fd15616c0cf218220b86d67b95f4
Antony Repin <egeshi@gmail.com>
2013-10-19 07:44:36 +0300

    Added controller for channels CRON jobs Removed Init() plugin, methods mooved to Bootstrap()

---
+++ application/modules/cron/controllers/ChannelsController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ChannelsControler
+ *
+ * @author Hiragana
+ */
+class ChannelsControler
+{
+    //put your code here
+}
+
+?>
