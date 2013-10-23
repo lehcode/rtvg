244c0f5f4da248d08484be7176a3fb0db9f1e302
Antony Repin <egeshi@gmail.com>
2013-10-23 17:49:30 +0300

    added getTodayBroadcasts()

--- application/models/ProgramsTest.php
+++ application/models/ProgramsTest.php
@@ -7,7 +7,7 @@
 class Xmltv_Model_ProgramsTest extends Xmltv_Model_Programs 
 {
     
-    public function thisWeekBroadcasts($channel_id=null, $week_start, $week_end){
+    public function thisWeekBroadcasts($channel_id=null, Zend_Date $week_start, Zend_Date $week_end){
         
         if (!$channel_id) {
             throw new Exception ('Channel ID not provided', 500);
@@ -20,6 +20,29 @@ class Xmltv_Model_ProgramsTest extends Xmltv_Model_Programs
             ->where("`EVT`.`start` < '".$week_end->toString("YYYY-MM-dd 23:59:59")."'")
             ->where("`EVT`.`channel` = ".$channel_id)
             ->group("BC.alias")
+        ;
+        
+        if (Zend_Registry::get('adult')!==true){
+            $select->where("`CH`.`adult` = FALSE");
+        }
+        
+        $result = $this->db->fetchAll($select);
+        return $result;
+        
+    }
+    
+    public function getTodayBroadcasts($channel_id=null){
+        
+        if (!$channel_id) {
+            throw new Exception ('Channel ID not provided', 500);
+        }
+        
+        $select = $this->db->select()
+            ->from(array('BC'=>$this->bcTable->getName()), 'alias')
+            ->joinLeft(array('EVT'=>$this->eventsTable->getName()), "`BC`.`hash`=`EVT`.`hash`", null)
+            ->where("`EVT`.`start` >= '".Zend_Date::now()->toString("YYYY-MM-dd 00:00:00")."'")
+            ->where("`EVT`.`start` < '".Zend_Date::now()->toString("YYYY-MM-dd 23:59:00")."'")
+            ->where("`EVT`.`channel` = ".$channel_id)
         ;
         
         if (Zend_Registry::get('adult')!==true){
