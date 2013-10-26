7b94a45847ac9db5b836e7c065d1d56a9523aad3
Antony Repin <egeshi@gmail.com>
2013-10-26 04:42:40 +0300

    Added YouTube categories table

---
+++ application/models/DbTable/YtCategories.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Youtube categories table
+ */
+class Xmltv_Model_DbTable_YtCategories extends Xmltv_Db_Table_Abstract
+{
+    protected $_name = 'yt_categories';
+    protected $_primary = 'title_en';
+	
+}
+?>
